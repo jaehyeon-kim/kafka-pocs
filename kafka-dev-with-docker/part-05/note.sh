09ac4456fa73d70158a59594ef3561c38d2fb2f2
Jaehyeon Kim <jaehyeon.kim@energyaustralia.com.au>
2023-05-17 16:44:34 +1000

    update note

--- kafka-dev-with-docker/part-05/note.sh
+++ kafka-dev-with-docker/part-05/note.sh
@@ -8,9 +8,9 @@ wget https://dlcdn.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bi
 export PATH="/opt/maven/bin:$PATH"
 
 ## build
-./download.sh
-cd plugins/aws-glue-schema-registry-v.1.1.15/build-tools
-mvn clean install -DskipTests -Dmaven.wagon.http.ssl.insecure=true
-cd ..
-mvn clean install -DskipTests
-mvn dependency:copy-dependencies
+echo "building glue schema registry..."
+cd plugins/$SOURCE_NAME/build-tools \
+  && mvn clean install -DskipTests -Dcheckstyle.skip \
+  && cd .. \
+  && mvn clean install -DskipTests -Dmaven.javadoc.skip=true \
+  && mvn dependency:copy-dependencies
