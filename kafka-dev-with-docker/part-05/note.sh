0e788d3d8f930761218a67d60e2c865d41d18222
Jaehyeon Kim <jaehyeon.kim@energyaustralia.com.au>
2023-05-17 16:11:50 +1000

    updating part 5

---
+++ kafka-dev-with-docker/part-05/note.sh
@@ -0,0 +1,16 @@
+## maven
+
+wget https://dlcdn.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.tar.gz \
+  && tar xvf apache-maven-3.8.8-bin.tar.gz \
+  && sudo mv apache-maven-3.8.8 /opt/maven \
+  && rm apache-maven-3.8.8-bin.tar.gz
+
+export PATH="/opt/maven/bin:$PATH"
+
+## build
+./download.sh
+cd plugins/aws-glue-schema-registry-v.1.1.15/build-tools
+mvn clean install -DskipTests -Dmaven.wagon.http.ssl.insecure=true
+cd ..
+mvn clean install -DskipTests
+mvn dependency:copy-dependencies
