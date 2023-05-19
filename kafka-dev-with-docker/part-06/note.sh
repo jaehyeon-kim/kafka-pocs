755d5ec5770b1b6dbfd830c491212970a0a9f584
Jaehyeon Kim <jaehyeon.kim@energyaustralia.com.au>
2023-05-19 12:26:54 +1000

    update note

--- kafka-dev-with-docker/part-06/note.sh
+++ kafka-dev-with-docker/part-06/note.sh
@@ -55,3 +55,30 @@ OS name: "linux", version: "5.4.72-microsoft-standard-wsl2", arch: "amd64", fami
 [INFO] Total time:  5.287 s
 [INFO] Finished at: 2023-05-19T08:08:51+10:00
 [INFO] ------------------------------------------------------------------------
+
+
+# "connectorConfiguration": {
+#     "connector.class": "io.confluent.connect.s3.S3SinkConnector",
+#     "s3.region": "ap-southeast-2",
+#     "format.class": "io.confluent.connect.s3.format.json.JsonFormat",
+#     "storage.class": "io.confluent.connect.s3.storage.S3Storage",
+#     "schema.compatibility": "BACKWARD",
+#     "tasks.max": "1",
+#     "flush.size": "2",
+#     "name": "confluent-s3-sink-avro",
+#     "topics.regex": "friends*",
+#     "s3.bucket.name": "<backup>",
+#     "key.converter.region": "ap-southeast-2",
+#     "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+#     "value.converter.region": "ap-southeast-2",
+#     "value.converter.registry.name": "testregistry",
+#     "value.converter": "com.amazonaws.services.schemaregistry.kafkaconnect.AWSKafkaAvroConverter",
+#     "value.converter.schemaAutoRegistrationEnabled": "true",
+#     "value.convertor.schemaName": "friends",
+#     "value.converter.dataFormat": "JSON",
+#     "value.converter.endpoint": "https://glue.ap-southeast-2.amazonaws.com",
+#     "value.converter.avroRecordType": "GENERIC_RECORD",
+#     "value.converter.schemas.enable": "true"
+# }
+
+# https://github.com/awslabs/aws-glue-schema-registry/issues/93
