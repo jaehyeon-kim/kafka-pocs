48424f46a5ce488bea5e518283c3a6ea46749640
Jaehyeon Kim <jaehyeon.kim@energyaustralia.com.au>
2023-05-30 15:17:01 +1000

    adding part 08

---
+++ kafka-dev-with-docker/part-08/generate.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+ 
+set -eu
+
+COUNTRY="${COUNTRY:-AU}"
+STATE="${STATE:-}"
+OU="${OU:-service-users}"
+CN="${CN:-kafka-admin}"
+LOCATION="${CITY:-}"
+PASSWORD="${PASSWORD:-supersecret}"
+
+VALIDITY_IN_DAYS=3650
+CA_WORKING_DIRECTORY="certificate-authority"
+TRUSTSTORE_WORKING_DIRECTORY="truststore"
+KEYSTORE_WORKING_DIRECTORY="keystore"
+CA_KEY_FILE="ca-key"
+CA_CERT_FILE="ca-cert"
+DEFAULT_TRUSTSTORE_FILE="kafka.truststore.jks"
+KEYSTORE_SIGN_REQUEST="cert-file"
+KEYSTORE_SIGN_REQUEST_SRL="ca-cert.srl"
+KEYSTORE_SIGNED_CERT="cert-signed"
+KAFKA_HOSTS_FILE="kafka-hosts.txt"
+ 
+if [ ! -f "$KAFKA_HOSTS_FILE" ]; then
+  echo "'$KAFKA_HOSTS_FILE' does not exists. Create this file"
+  exit 1
+fi
+ 
+echo "Welcome to the Kafka SSL certificate authority, key store and trust store generator script."
+
+echo
+echo "Two files will be created if not existing:"
+echo " - $CA_WORKING_DIRECTORY/$CA_KEY_FILE -- the private key used later to sign certificates"
+echo " - $CA_WORKING_DIRECTORY/$CA_CERT_FILE -- the certificate that will be"
+echo " stored in the trust store in a moment and serve as the certificate authority (CA)."
+echo
+if [ -f "$CA_WORKING_DIRECTORY/$CA_KEY_FILE" ] && [ -f "$CA_WORKING_DIRECTORY/$CA_CERT_FILE" ]; then
+  echo "Use existing $CA_WORKING_DIRECTORY/$CA_KEY_FILE and $CA_WORKING_DIRECTORY/$CA_CERT_FILE ..."
+else
+  rm -rf $CA_WORKING_DIRECTORY && mkdir $CA_WORKING_DIRECTORY
+  echo
+  echo "Generate $CA_WORKING_DIRECTORY/$CA_KEY_FILE and $CA_WORKING_DIRECTORY/$CA_CERT_FILE ..."
+  echo
+  openssl req -new -newkey rsa:4096 -days $VALIDITY_IN_DAYS -x509 \
+    -subj "/C=$COUNTRY/ST=$STATE/L=$LOCATION/O=$OU/CN=$CN" \
+    -keyout $CA_WORKING_DIRECTORY/$CA_KEY_FILE \
+    -out $CA_WORKING_DIRECTORY/$CA_CERT_FILE -nodes
+fi
+
+echo
+echo "Now the trust store will be generated from the certificate."
+echo
+rm -rf $TRUSTSTORE_WORKING_DIRECTORY && mkdir $TRUSTSTORE_WORKING_DIRECTORY
+keytool -keystore $TRUSTSTORE_WORKING_DIRECTORY/$DEFAULT_TRUSTSTORE_FILE \
+  -alias CARoot -import -file $CA_WORKING_DIRECTORY/$CA_CERT_FILE \
+  -noprompt -dname "C=$COUNTRY, ST=$STATE, L=$LOCATION, O=$OU, CN=$CN" -keypass $PASSWORD -storepass $PASSWORD
+
+echo
+echo "A keystore will be generated for each host in $KAFKA_HOST as each broker and logical client needs its own keystore"
+echo
+echo " NOTE: currently in Kafka, the Common Name (CN) does not need to be the FQDN of"
+echo " this host. However, at some point, this may change. As such, make the CN"
+echo " the FQDN. Some operating systems call the CN prompt 'first / last name'" 
+echo " To learn more about CNs and FQDNs, read:"
+echo " https://docs.oracle.com/javase/7/docs/api/javax/net/ssl/X509ExtendedTrustManager.html"
+rm -rf $KEYSTORE_WORKING_DIRECTORY && mkdir $KEYSTORE_WORKING_DIRECTORY
+ while read -r KAFKA_HOST || [ -n "$KAFKA_HOST" ]; do
+  KEY_STORE_FILE_NAME="$KAFKA_HOST.server.keystore.jks"
+  echo
+  echo "'$KEYSTORE_WORKING_DIRECTORY/$KEY_STORE_FILE_NAME' will contain a key pair and a self-signed certificate."
+  keytool -genkey -keystore $KEYSTORE_WORKING_DIRECTORY/"$KEY_STORE_FILE_NAME" \
+    -alias localhost -validity $VALIDITY_IN_DAYS -keyalg RSA \
+    -noprompt -dname "C=$COUNTRY, ST=$STATE, L=$LOCATION, O=$OU, CN=$KAFKA_HOST" \
+    -keypass $PASSWORD -storepass $PASSWORD
+ 
+  echo
+  echo "Now a certificate signing request will be made to the keystore."
+  keytool -certreq -keystore $KEYSTORE_WORKING_DIRECTORY/"$KEY_STORE_FILE_NAME" \
+    -alias localhost -file $KEYSTORE_SIGN_REQUEST -keypass $PASSWORD -storepass $PASSWORD
+ 
+  echo
+  echo "Now the trust store's private key (CA) will sign the keystore's certificate."
+  openssl x509 -req -CA $CA_WORKING_DIRECTORY/$CA_CERT_FILE \
+    -CAkey $CA_WORKING_DIRECTORY/$CA_KEY_FILE \
+    -in $KEYSTORE_SIGN_REQUEST -out $KEYSTORE_SIGNED_CERT \
+    -days $VALIDITY_IN_DAYS -CAcreateserial
+  # creates $CA_WORKING_DIRECTORY/$KEYSTORE_SIGN_REQUEST_SRL which is never used or needed.
+ 
+  echo
+  echo "Now the CA will be imported into the keystore."
+  keytool -keystore $KEYSTORE_WORKING_DIRECTORY/"$KEY_STORE_FILE_NAME" -alias CARoot \
+    -import -file $CA_WORKING_DIRECTORY/$CA_CERT_FILE -keypass $PASSWORD -storepass $PASSWORD -noprompt
+ 
+  echo
+  echo "Now the keystore's signed certificate will be imported back into the keystore."
+  keytool -keystore $KEYSTORE_WORKING_DIRECTORY/"$KEY_STORE_FILE_NAME" -alias localhost \
+    -import -file $KEYSTORE_SIGNED_CERT -keypass $PASSWORD -storepass $PASSWORD
+
+  echo
+  echo "All done!"
+  echo
+  echo "Deleting intermediate files. They are:"
+  echo " - '$CA_WORKING_DIRECTORY/$KEYSTORE_SIGN_REQUEST_SRL': CA serial number"
+  echo " - '$KEYSTORE_SIGN_REQUEST': the keystore's certificate signing request"
+  echo " - '$KEYSTORE_SIGNED_CERT': the keystore's certificate, signed by the CA, and stored back"
+  echo " into the keystore"
+  rm -f $CA_WORKING_DIRECTORY/$KEYSTORE_SIGN_REQUEST_SRL $KEYSTORE_SIGN_REQUEST $KEYSTORE_SIGNED_CERT
+done < "$KAFKA_HOSTS_FILE"
