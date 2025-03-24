ba078671bcfaad33a69eadb0efe033ab50c9d207
Jaehyeon Kim <dottami@gmail.com>
2025-03-25 10:47:46 +1100

    add lakehouse

--- factorhouse-local/note.sh
+++ factorhouse-local/note.sh
@@ -3,5 +3,9 @@ docker-compose -f kpow-trial.yml up -d \
 
 docker-compose -f kpow-trial.yml down --remove-orphans -v
 
+docker-compose -f lakehouse.yml up -d
+# https://blog.min.io/a-developers-introduction-to-apache-iceberg-using-minio/
+# https://github.com/databricks/docker-spark-iceberg
+
 # flex pricing per server or per workload?
 # FLINK_REST_URL=http://jobmanager:8081 ## fail without protocol eg) jobmanager:8081
